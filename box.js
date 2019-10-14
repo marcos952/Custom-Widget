68a5116b7c9c76e1488e5a098859d8aba4aa7dde
marcos952 <56541247+marcos952@users.noreply.github.com>
2019-10-14 12:48:39 +0200

    Update box.js

--- box.js
+++ box.js
@@ -1,7 +1,7 @@
 (function() { 
 	let template = document.createElement("template");
 	template.innerHTML = `
-                <input type="submit" value="Active me¡">
+                <input type="submit" value="Active me">
 		
 	`;
 
