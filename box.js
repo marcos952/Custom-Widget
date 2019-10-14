5e32b60c34cdef2ff6d8bf1b63d9fdcea3d14751
marcos952 <56541247+marcos952@users.noreply.github.com>
2019-10-14 12:31:11 +0200

    Add files via upload

---
+++ box.js
@@ -0,0 +1,35 @@
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+                <input type="submit" value="Active me¡">
+		
+	`;
+
+	class Box extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			this._props = {};
+		}
+
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("color" in changedProperties) {
+				this.style["background-color"] = changedProperties["color"];
+			}
+			if ("opacity" in changedProperties) {
+				this.style["opacity"] = changedProperties["opacity"];
+			}
+		}
+	}
+
+	customElements.define("com-sample-box", Box);
+})();
