305ce3216009f275d3d9933ca896118632ed8400
marcos952 <56541247+marcos952@users.noreply.github.com>
2019-10-16 16:45:16 +0200

    Update button.js

--- button.js
+++ button.js
@@ -43,6 +43,10 @@
 			if ("opacity" in changedProperties) {
 				this.style["opacity"] = changedProperties["opacity"];
 			}
+			
+			if ("colorinactive" in changedProperties) {
+				this.style["background-color"] = changedProperties["colorinactive"];
+			}
 
 			
 		}
